c18c95097dc7fdc8691cdb7cb28d382181fa8937
sami12-star <samiasifulalam@gmail.com>
2026-05-01 21:28:56 +0600

    initial commit: project structure and basic program

---
+++ README.md
@@ -0,0 +1,3 @@
+Project Title: A simple Project
+My name is Sami
+this project shows us how to use git and guthub dude
---
+++ src/main.py
@@ -0,0 +1,4 @@
+#write todays name
+print("Today's name is Sami")
+#write todays dateprint("Today's date is 2024-06-01")
+print("Today's date is 2024-06-01")
